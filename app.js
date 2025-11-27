352fc01b550e17a665dc4c210342ed1bf6bd34f2
suraj vishwakarma <dev.surajv@gmail.com>
2025-11-27 22:07:50 +0530

    feat: add rename functionality and remove data.txt

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ import fs from 'fs'
 const app = express();
 
 app.get('/',(req,res)=>{
-
     res.send("welcome to my server")
-
 })
 
 app.get("/sign",(req,res)=>{
@@ -19,6 +17,17 @@ app.get("/sign",(req,res)=>{
     res.send("file is created ")
 });
 
+
+app.get("/rename",(req,res)=>{
+    const {oldFile} =req.query;
+    const {newFile}= req.query;
+    fs.rename(oldFile, newFile, (err) => {
+      if (err) throw err;
+      console.log("file renamed");
+    });
+
+    res.send("file renamed successfully");
+});
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
